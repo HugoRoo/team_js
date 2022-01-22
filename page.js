915cb66d19882fbaa88f5e06dd386668d6b70667
HugoRo <ndhugohro@gmail.com>
2022-01-21 21:01:39 -0600

    add new changes

--- page.js
+++ page.js
@@ -6,12 +6,12 @@ document.body.appendChild(divGeneral);
 
 
 //start element tittle in of divGeneral
-let titlePage= document.createElement(h1);
+let titlePage= document.createElement("h1");
 titlePage.innerText= "The Best Smartphones of the year";
 divGeneral.appendChild(titlePage);
 
 //Second element subtittle in of divGeneral
-let subtitlePage= document.createElement(h2);
+let subtitlePage= document.createElement("h2");
 subtitlePage.innerText= "ALWAYS BELIEVE THAT SOMETHING WONDERFUL IS ABOUT TO HAPPEN";
 divGeneral.appendChild(subtitlePage);
 
@@ -29,3 +29,9 @@ let description1 = "Con la cámara del nuevo Redmi Note 10 Pro Explora tu mundo 
 let description2 = "Con la cámara del nuevo Redmi Note 10 Pro Explora tu mundo con 108MP \n Sistema de imágenes a nivel de nivel insignia, súper claro, súper emocionante.\n Cámara cuádruple de 108MP gran angular + 8MP Cámara gran angular + 5MP Cámara telemacro + 2MP Sensor de profundidad. Y Cámara Frontal de 16MP\n La pantalla AMOLED de 6,67 inch. del Redmi Note 10 Pro le brinda una fluidez impecable y una respuesta instantánea.La pantalla se actualiza 120 veces por segundo, tanto si juegas como si te desplazas por el contenido, el aspecto y la sensación son extraordinariamente suaves y fluidos.";
 
 let description3 = "Con la cámara del nuevo Redmi Note 10 Pro Explora tu mundo con 108MP \n Sistema de imágenes a nivel de nivel insignia, súper claro, súper emocionante.\n Cámara cuádruple de 108MP gran angular + 8MP Cámara gran angular + 5MP Cámara telemacro + 2MP Sensor de profundidad. Y Cámara Frontal de 16MP\n La pantalla AMOLED de 6,67 inch. del Redmi Note 10 Pro le brinda una fluidez impecable y una respuesta instantánea.La pantalla se actualiza 120 veces por segundo, tanto si juegas como si te desplazas por el contenido, el aspecto y la sensación son extraordinariamente suaves y fluidos.";
+
+//Cards
+
+let card_1= document.createElement("card_1");
+card_1.box= "description_1";
+divGeneral.appendChild(titlePage);
