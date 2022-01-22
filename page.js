96f8b530257cbf3806c3b0fef45a1fe2ed11e317
HugoRo <ndhugohro@gmail.com>
2022-01-21 20:37:49 -0600

    add new change of the page

---
+++ page.js
@@ -0,0 +1,31 @@
+
+//Create element divGeneral
+
+let divGeneral = document.createElement("div");
+document.body.appendChild(divGeneral);
+
+
+//start element tittle in of divGeneral
+let titlePage= document.createElement(h1);
+titlePage.innerText= "The Best Smartphones of the year";
+divGeneral.appendChild(titlePage);
+
+//Second element subtittle in of divGeneral
+let subtitlePage= document.createElement(h2);
+subtitlePage.innerText= "ALWAYS BELIEVE THAT SOMETHING WONDERFUL IS ABOUT TO HAPPEN";
+divGeneral.appendChild(subtitlePage);
+
+
+//Types
+
+let smartphone1 = "REDMI NOTE 10 Pro Unlocked";
+let smartphone2 = "REDMI 10 Unlocked";
+let smartphone3 = "REDMI 9T Unlocked";
+
+//Descriptions
+
+let description1 = "Con la cámara del nuevo Redmi Note 10 Pro Explora tu mundo con 108MP \n Sistema de imágenes a nivel de nivel insignia, súper claro, súper emocionante.\n Cámara cuádruple de 108MP gran angular + 8MP Cámara gran angular + 5MP Cámara telemacro + 2MP Sensor de profundidad. Y Cámara Frontal de 16MP\n La pantalla AMOLED de 6,67 inch. del Redmi Note 10 Pro le brinda una fluidez impecable y una respuesta instantánea.La pantalla se actualiza 120 veces por segundo, tanto si juegas como si te desplazas por el contenido, el aspecto y la sensación son extraordinariamente suaves y fluidos.";
+
+let description2 = "Con la cámara del nuevo Redmi Note 10 Pro Explora tu mundo con 108MP \n Sistema de imágenes a nivel de nivel insignia, súper claro, súper emocionante.\n Cámara cuádruple de 108MP gran angular + 8MP Cámara gran angular + 5MP Cámara telemacro + 2MP Sensor de profundidad. Y Cámara Frontal de 16MP\n La pantalla AMOLED de 6,67 inch. del Redmi Note 10 Pro le brinda una fluidez impecable y una respuesta instantánea.La pantalla se actualiza 120 veces por segundo, tanto si juegas como si te desplazas por el contenido, el aspecto y la sensación son extraordinariamente suaves y fluidos.";
+
+let description3 = "Con la cámara del nuevo Redmi Note 10 Pro Explora tu mundo con 108MP \n Sistema de imágenes a nivel de nivel insignia, súper claro, súper emocionante.\n Cámara cuádruple de 108MP gran angular + 8MP Cámara gran angular + 5MP Cámara telemacro + 2MP Sensor de profundidad. Y Cámara Frontal de 16MP\n La pantalla AMOLED de 6,67 inch. del Redmi Note 10 Pro le brinda una fluidez impecable y una respuesta instantánea.La pantalla se actualiza 120 veces por segundo, tanto si juegas como si te desplazas por el contenido, el aspecto y la sensación son extraordinariamente suaves y fluidos.";
